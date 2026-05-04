8e7a743a0a7f233bcee111bdfb8e49485a52137f
mangoog <mjnhdi35@gmail.com>
2026-05-05 05:33:20 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import config from './config/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = config().port;
+  await app.listen(port);
 }
+
 bootstrap();
